4dde565bf156eaf75d0546aadfaef5184a438b23
CookK_S <vital2486@naver.com>
2020-10-21 21:06:04 +0900

    LogController

--- src/com/konkuk/controller/LogController.java
+++ src/com/konkuk/controller/LogController.java
@@ -1,13 +1,7 @@
 package com.konkuk.controller;
 
-import com.konkuk.controller.EmployeeController.Menu;
-import com.konkuk.repository.Repository;
-import com.konkuk.service.UI;
 
-import java.io.FileNotFoundException;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import com.konkuk.service.LogService;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
@@ -19,7 +13,7 @@ public class LogController extends Controller {
     public LogController(Menu menu) {
         this.currentMenu = menu;
     }
-    // EmployeeController와 그냥 동일하게 만들었음. MainController에서 case 3 : next = new LogController(LogController.Menu.LOG); 필요
+
     public Controller start() {
         if (currentMenu == Menu.LOG) {
             try {
@@ -34,40 +28,29 @@ public class LogController extends Controller {
         return new MainController();
     }
 
-    public static void Print_Log() {
-        Repository rp = new Repository();
-        try {
-            File file = new File("log.txt");
-            FileReader filereader = new FileReader(file);
-            BufferedReader bufReader = new BufferedReader(filereader);
-            String line = "";
-            System.out.println("[로그내역]");
-            System.out.println("-----------------------------------------------------------------------------------------");
-            while((line = bufReader.readLine()) != null) {
-                List<String> parsed = rp.parseDataLine(line); //Repository에 parseDataLine을 protected -> public 수정 필요
-                String s = parsed.get(3);
-                String[] arr;
-                arr = s.split("");
-                for (int i = 0; i < arr.length; i++) {
-                    System.out.print(arr[i]);
-                    if (i == 3 || i == 5)
-                    {
-                        System.out.print("/");
-                    }
-
+    private static void Print_Log() {
+        List<String> log_list = LogService.get_log();
+        System.out.println("[로그내역]");
+        System.out.println("-----------------------------------------------------------------------------------------");
+        for (int i=0; i<log_list.size(); i += 4) {
+            String s = log_list.get(i+3);
+            String[] arr;
+            arr = s.split("");
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[j]);
+                if (j == 3 || j == 5)
+                {
+                    System.out.print("/");
                 }
-                System.out.print(" [" + parsed.get(1) + "] ");
-                System.out.println(parsed.get(2));
             }
-            bufReader.close();
-            System.out.println("-----------------------------------------------------------------------------------------");
-            System.out.print("이전 화면으로 돌아가시려면 'B', 'b'를 입력해주세요 : ");
-        }catch (FileNotFoundException e) {
-
-        }catch (IOException e) {
-
+            System.out.print(" " + log_list.get(i+1) + " ");
+            System.out.println(log_list.get(i+2) + " ");
         }
+
+        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.print("이전 화면으로 돌아가시려면 'B', 'b'를 입력해주세요 : ");
     }
+
 
     public static void LogCheck() throws IOException, InterruptedException {
         while(true) {
