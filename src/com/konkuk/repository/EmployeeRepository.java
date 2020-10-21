efb5e1ec33d7333f06033506014a07bb6ed45c7e
short-term <shortterm@kakao.com>
2020-10-21 13:44:35 +0900

    [Repository] Employee 더미 메소드 추가

--- src/com/konkuk/repository/EmployeeRepository.java
+++ src/com/konkuk/repository/EmployeeRepository.java
@@ -4,6 +4,8 @@ import com.konkuk.Utils;
 import com.konkuk.asset.Langs;
 import com.konkuk.asset.Settings;
 import com.konkuk.dto.Employee;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeRepository extends Repository {
@@ -12,13 +14,13 @@ public class EmployeeRepository extends Repository {
 
     private EmployeeRepository() {
         this.debugTitle = "Employee";
-        if(isDataFileExists(Settings.DATA_EMPLOYEE)) {
+        if (isDataFileExists(Settings.DATA_EMPLOYEE)) {
             employeeList = loadData(Settings.DATA_EMPLOYEE, (parsedData, uniquePolicy) -> {
                 int id = Integer.parseInt(parsedData.get(0));
                 String name = parsedData.get(1);
                 int salary = Integer.parseInt(parsedData.get(2));
                 int residualDayOff = Integer.parseInt(parsedData.get(3));
-                if(uniquePolicy.contains(id)) {
+                if (uniquePolicy.contains(id)) {
                     Utils.exit(Langs.VIOLATE_UNIQUE_KEY);
                 }
                 return new Employee(id, name, salary, residualDayOff);
@@ -36,12 +38,50 @@ public class EmployeeRepository extends Repository {
         return Instance.instance;
     }
 
+    // todo: 단기 - 껍데기만 만들어둠
     private int maxId = -1;
-    public void add(Employee employee) {
-        if(maxId == -1) {
+    public Employee add(Employee employee) {
+        if (maxId == -1) {
             employeeList.forEach((e -> maxId = Math.max(maxId, e.id)));
         }
         employee.id = ++maxId;
         // employee save
+        return employee;
+    }
+
+    // todo: 단기 - 껍데기만 만들어둠, 부분 문자열 검색
+    public List<Employee> findByName(String name) {
+        List<Employee> results = new ArrayList<>();
+        results.add(new Employee(0, "임시", 1000, 5));
+        return results;
+    }
+
+    // todo: 단기 - 껍데기만 만들어둠색, 완전 일치 검색
+    public List<Employee> findBySalary(int salary) {
+        List<Employee> results = new ArrayList<>();
+        results.add(new Employee(0, "임시", 1000, 5));
+        return results;
+    }
+
+    // todo: 단기 - 껍데기만 만들어둠, 부분 문자열 검색
+    public List<Employee> findById(int id) {
+        List<Employee> results = new ArrayList<>();
+        results.add(new Employee(0, "임시", 1000, 5));
+        return results;
+    }
+
+    // todo: 단기 - 껍데기만 만들어둠, 완전 일치 검색
+    public Employee findByExactId(int id) {
+        return new Employee(0, "임시", 1000, 5);
+    }
+
+    // todo: 단기 - 껍데기만 만들어둠
+    public Employee update(int targetId, Employee datas) {
+        return datas;
+    }
+
+    // todo: 단기 - 껍데기만 만들어둠
+    public Boolean delete(int targetId) {
+        return true;
     }
 }
