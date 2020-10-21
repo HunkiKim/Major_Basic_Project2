3cf75f7bf1792bbd0334aebe06fbda968a278c90
short-term <shortterm@kakao.com>
2020-10-21 13:59:45 +0900

    [DTO] DayOff에서 날짜 String -> Date로 변경

--- src/com/konkuk/dto/DayOff.java
+++ src/com/konkuk/dto/DayOff.java
@@ -2,22 +2,24 @@ package com.konkuk.dto;
 
 import com.konkuk.asset.Langs;
 
+import java.util.Date;
+
 public class DayOff {
     public int num;
     public int id;
     public int dayOffNumber;
     public String reason;
-    public String startDayOffNumber;
-    public String endDayOffNumber;
-    public String dateCreated;
+    public Date dateDayOffStart;
+    public Date dateDayOffEnd;
+    public Date dateCreated;
 
-    public DayOff(int num, int id, int dayOffNumber, String reason, String startDayOffNumber, String endDayOffNumber, String dateCreated) {
+    public DayOff(int num, int id, int dayOffNumber, String reason, Date dateDayOffStart, Date dateDayOffEnd, Date dateCreated) {
         this.num = num;
         this.id = id;
         this.dayOffNumber = dayOffNumber;
         this.reason = reason;
-        this.startDayOffNumber = startDayOffNumber;
-        this.endDayOffNumber = endDayOffNumber;
+        this.dateDayOffStart = dateDayOffStart;
+        this.dateDayOffEnd = dateDayOffEnd;
         this.dateCreated = dateCreated;
     }
     public static String getHeader(){
