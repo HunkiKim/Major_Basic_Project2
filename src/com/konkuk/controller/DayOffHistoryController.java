377bb0504c62469720171862d8ba2f39bd132311
wildDoubt <coma01155@gmail.com>
2020-10-22 05:55:11 +0900

    연차 내역 조회 Service 및 Controller 수정

--- src/com/konkuk/controller/DayOffHistoryController.java
+++ src/com/konkuk/controller/DayOffHistoryController.java
@@ -7,10 +7,16 @@ import com.konkuk.dto.DayOff;
 import com.konkuk.service.DayOffHistoryService;
 
 public class DayOffHistoryController extends Controller{
+    public int annualPage;
+    public int searchPage;
+    public DayOffHistoryController(){
+        this.annualPage = 0;
+        this.searchPage = 0;
+    }
     public enum Menu{
         ANNUAL_HISTORY("1"),
         ADVANCED_SEARCH("2");
-
+        //"b", "B" 추가
         private String name;
 
         Menu(String menuName){
@@ -20,24 +26,76 @@ public class DayOffHistoryController extends Controller{
             return name;
         }
     }
+
+    public enum PageMenu{
+        NEXT_PAGE("1"),
+        PREV_PAGE("2");
+        //"b", "B" 추가
+        private String name;
+
+        PageMenu(String menuName){
+            this.name = menuName;
+        }
+        String getMenu(){
+            return name;
+        }
+    }
+
     public Controller start(){
 
         UI.print(Langs.DAY_OFF_HISTORY_MAIN);
         String menu = UI.getInput();
         if(menu.equals(Menu.ANNUAL_HISTORY.getMenu())){
-            anuualHistory();
+            annualHistory();
         } else if(menu.equals(Menu.ADVANCED_SEARCH.getMenu())){
-            adnvancedSearch();
+            advancedSearch();
         }
         // back 메뉴 처리
 
 
         return new MainController();
     }
-    public void anuualHistory(){
+    public Controller annualHistory(){
         //연간 연차내역 조회
+        boolean result;
+
+        DayOffHistoryService history = new DayOffHistoryService();
+        result = history.annualHistory(this.annualPage);
+        if(!result){
+            UI.print("해당 연차내역이 존재하지 않습니다.\n"); //Langs에 추가
+            return new DayOffHistoryController();
+        }
+
+        UI.print(Langs.DAY_OFF_HISTORY_PAGE);
+        String menu = UI.getInput();
+
+        if(menu.equals(PageMenu.NEXT_PAGE.getMenu())){
+            // back -> DayOffHistoryController
+            if(history.isExistsNextPage(this.annualPage)) this.annualPage++;
+            else{
+                UI.print("마지막 페이지입니다.");//Langs에 추가
+                this.annualHistory();
+            }
+        }else if(menu.equals(PageMenu.PREV_PAGE.getMenu())){
+            // back -> DayOffHistoryController
+            if(history.isExistsPrevPage(this.annualPage)) this.annualPage--;
+            else{
+                UI.print("첫 페이지입니다.");// Langs에 추가
+                this.annualHistory();
+            }
+        }
+        return new DayOffHistoryController();
     }
-    public void adnvancedSearch(){
+    public Controller advancedSearch(){
         //연차내역 상세 검색
+        boolean result;
+
+        DayOffHistoryService history = new DayOffHistoryService();
+        result = history.advancedSearch(this.searchPage);
+        if(!result){
+            UI.print("해당 연차내역이 존재하지 않습니다.\n"); // Langs에 추가
+            return new DayOffHistoryController();
+        }
+        return new DayOffHistoryController();
     }
 }
--- src/com/konkuk/service/DayOffHistoryService.java
+++ src/com/konkuk/service/DayOffHistoryService.java
@@ -1,12 +1,26 @@
 package com.konkuk.service;
 
 import com.konkuk.dto.DayOff;
+import com.konkuk.repository.DayOffHistoryRepository;
+import com.konkuk.repository.DayOffRepository;
 
 public class DayOffHistoryService {
-    public boolean anuualHistory(){
+    DayOffHistoryRepository dayOffHistoryRepository = DayOffHistoryRepository.getInstance();
+
+    public boolean annualHistory(int annualPage) {
+        // 해당 페이지의 연간 연차내역 출력
         return false;
     }
-    public boolean advancedSearch(){
+
+    public boolean advancedSearch(int searchPage){
         return false;
+    }
+
+    public boolean isExistsNextPage(int annualPage) {
+        return annualPage < dayOffHistoryRepository.getSize();
+    }
+
+    public boolean isExistsPrevPage(int annualPage) {
+        return annualPage == 0;
     }
 }
