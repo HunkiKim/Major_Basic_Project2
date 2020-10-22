afb94dea67885254da85db7988f3a7a28a5eb212
김훈기 <gnsrl76@naver.com>
2020-10-22 17:53:36 +0900

    중간 저장 (직원 add)

--- src/com/konkuk/service/EmployeeService.java
+++ src/com/konkuk/service/EmployeeService.java
@@ -1,11 +1,59 @@
 package com.konkuk.service;
 
+import com.konkuk.UI;
+import com.konkuk.asset.Langs;
 import com.konkuk.dto.Employee;
+
+import java.util.regex.Pattern;
 
 public class EmployeeService {
     public boolean add(Employee employee) {
         // 비즈니스 로직
 
         return false;
+    }
+    public boolean idcheck(String id){
+        for (int i = 0; i < id.length(); i++) { //숫자가 아닐시
+            if (id.charAt(i) < 48 || id.charAt(i) > 58) {
+                UI.print(Langs.LETTER_ERROR);
+                return false;
+            }
+        }
+        //길이 확인
+        if(id.length()<1 || id.length()>7){
+            UI.print(Langs.LENGTH_ERROR);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean namecheck(String name){
+        if(!Pattern.matches("^[a-zA-Z가-힣]*$",name)){//정규표현식 한글
+            UI.print(Langs.LETTER_ERROR);
+            return false;
+        }
+
+        if(name.getBytes().length<1 || name.getBytes().length>32) { //한글 영어 둘다있기때문에 byte로 했습니다.
+            UI.print(Langs.LETTER_ERROR);
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean salarycheck(String salary){
+        for (int i = 0; i < salary.length(); i++) { //숫자가 아닐시
+            if (salary.charAt(i) < 48 || salary.charAt(i) > 58) {
+                UI.print(Langs.LETTER_ERROR);
+                return false;
+            }
+        }
+
+        if(salary.length()>=8 && salary.length()<=10){
+            UI.print(Langs.LENGTH_ERROR);
+            return false;
+        }
+
+        return true;
     }
 }
