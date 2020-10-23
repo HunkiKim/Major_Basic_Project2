67124b3d5f38879fdef712141f98f729ff8d835b
CookK_S <vital2486@naver.com>
2020-10-23 20:22:30 +0900

    불필요 요소 삭제

--- src/com/konkuk/controller/LogController.java
+++ src/com/konkuk/controller/LogController.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.List;
-import java.util.Scanner;
 
 public class LogController extends Controller {
     public enum Menu {LOG}
