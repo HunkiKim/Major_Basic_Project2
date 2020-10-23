3edef5fc63a5bbab6f6e1e9193f8df8afce40a1f
CookK_S <vital2486@naver.com>
2020-10-23 20:23:33 +0900

    .

--- src/com/konkuk/asset/Settings.java
+++ src/com/konkuk/asset/Settings.java
@@ -4,6 +4,5 @@ public final class Settings {
     public static final String DATA_EMPLOYEE = "./employee.txt";
     public static final String DATA_LOG = "./log.txt";
     public static final String DATA_DAYOFF = "./dayoff.txt";
-    public static final String DATA_LOG = "./log.txt";
     public static final Boolean DEBUG = true;
 }
