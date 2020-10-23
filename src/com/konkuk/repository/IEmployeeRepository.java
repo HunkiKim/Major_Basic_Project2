fcf41852bd7ba50f519c42c00fd6727bf2fd069d
short-term <shortterm@kakao.com>
2020-10-23 16:53:56 +0900

    employeeRepository 작업

--- src/com/konkuk/repository/IEmployeeRepository.java
+++ src/com/konkuk/repository/IEmployeeRepository.java
@@ -2,14 +2,15 @@ package com.konkuk.repository;
 
 import com.konkuk.dto.Employee;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface IEmployeeRepository {
-    public Employee add(Employee employee);
+    public Employee add(Employee employee) throws IOException;
     public List<Employee> findByName(String name);
     public List<Employee> findBySalary(int salary);
     public List<Employee> findById(int id);
     public Employee findByExactId(int id);
-    public Employee update(int targetId, Employee datas);
-    public Boolean delete(int targetId);
+    public Employee update(int targetId, Employee data) throws IOException;
+    public void delete(int targetId) throws IOException;
 }
