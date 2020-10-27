fec1c0cceb43dadbb57cdf6b9ccb3a87b99991aa
CookK_S <vital2486@naver.com>
2020-10-27 17:31:23 +0900

    출력 수정.

--- src/com/konkuk/controller/LogController.java
+++ src/com/konkuk/controller/LogController.java
@@ -35,9 +35,9 @@ public class LogController extends Controller {
     private void Print_Log() {
         List<Log> log_list = (new LogService()).getLogs();
         UI.print(Langs.LOG_TITLE);
-        UI.print(Langs.HORIZON);
+        UI.print2(Langs.HORIZON);
         log_list.forEach((log -> UI.print(log.Day.substring(0, 4) + "/" + log.Day.substring(4, 6) + "/" + log.Day.substring(6,14) + " " + log.log_category + log.log_content)));
-        UI.print(Langs.HORIZON);
+        UI.print2(Langs.HORIZON);
         UI.print(Langs.INSERT_BACK);
     }
 
@@ -52,11 +52,9 @@ public class LogController extends Controller {
                 break;
             }
             else {
-                System.out.print("올바르지 않은 입력입니다. 다시 입력해주세요");
+                UI.print2("올바르지 않은 입력입니다. 다시 입력해주세요");
                 try {
                     Thread.sleep(2000);
-                    System.out.print("\033[H\033{2J");
-                    System.out.flush();
                 } catch(InterruptedException e) {
                     e.printStackTrace();
                 }
